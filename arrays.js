9965dd136c627e08a038fc9ec6d885d5a3d92c1f
Moustafa Mohamed <musta7188@hotmail.it>
2019-12-04 18:39:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ function addElementToBeginningOfArray(array, element){
 
 function destructivelyAddElementToBeginningOfArray(array, element){
 
-  array.push(`${element}`);
+  array.push(element);
   return array;
 
 
