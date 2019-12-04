43e5e62225b9a0054c0a80376d016cd9fc897b94
Moustafa Mohamed <musta7188@hotmail.it>
2019-12-04 18:44:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element){
 
   var newArray = array.push(element);
-  
+
   return array;
 
 
